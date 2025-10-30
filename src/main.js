1dcbad0f084d6a3ea1def8f19d60ccc0beb33175
bayufadayan <bayufadayan@gmail.com>
2025-10-30 18:47:17 +0700

    chore: change import using @ alias

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
-import './assets/style/main.css';
+import App from '@/App.vue';
+import router from '@/router';
+import store from '@/store';
+import '@/assets/style/main.css';
 
 Vue.config.productionTip = false
 
